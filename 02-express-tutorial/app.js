73a1fca6d026ac9bf5f25f3e1405eaf138438764
npadmanabhan <nandu77@gmail.com>
2024-02-12 06:35:54 -0600

    Various commits

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,6 +1,20 @@
 const express = require('express')
 const app = express()
 
+app.get('/', (req,res)=> {
+    console.log('User hit the home page resource')
+    res.send('Home Page')
+})
+
+app.get('/about', (req,res)=> {
+    console.log('User hit the about page resource')
+    res.status(200).send('About Page')
+})
+
+app.all('*',(req,res)=> {
+    res.status(404).send('<h1>Resource not found</h1>')
+})
+
 app.listen(5000, ()=> {
     console.log('server is listening on port 5000')
     console.log('waiting for requests to come in')
