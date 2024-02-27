874ec1dee0415d3687a40990054887b266907cb5
Nandu Padmanabhan <nandu77@gmail.com>
2024-02-27 08:53:56 -0600

    Various task manager changes

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,10 +1,17 @@
 const express = require('express')
 const app = express()
+const tasks = require('./routes/tasks')
+const port = 3000
+
+//Middlware
+app.use(express.json())
 
 app.get('/hello', (req, res) => {
     res.status(200).send('Task Manager App')
 })
-const port = 3000
+
+app.use('/api/v1/tasks', tasks)
+
 
 app.listen(port, console.log(`Server is listening on port ${port}...`))
 console.log('Task Manager App')
