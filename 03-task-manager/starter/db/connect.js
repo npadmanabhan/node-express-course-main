bc39fa9b2bf9a6825b8bb7a4d6ddc58373ae616c
Nandu Padmanabhan <nandu77@gmail.com>
2024-02-28 21:28:57 -0600

    Remove DB file

--- 03-task-manager/starter/db/connect.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-
-const connectionString = 'mongodb+srv://app_user:xxxxxxxxxx@nodejs-task-manager.croec03.mongodb.net/?retryWrites=true&w=majority&appName=NodeJS-Task-Manager'
-
-mongoose
-.connect(connectionString)
-.then(()=> console.log('Connected to the Database...'))
-.catch((err)=> console.log(err))
