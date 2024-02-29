b62c3bab995e1c9427c7304d74606a81b0484ba3
Nandu Padmanabhan <nandu77@gmail.com>
2024-02-29 08:47:06 -0600

    added model

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -1,10 +1,16 @@
+const Task = require('../models/tasks')
 
 const getAllTasks = (req,res)=>{
     res.send('Get All Tasks')
 }
 
-const createTask = (req,res)=>{
-    res.json(req.body)
+// const createTask = (req,res)=>{
+//     res.json(req.body)
+// }
+
+const createTask = async (req,res)=> {
+    const task = await Task.create(req.body)
+    res.status(201).json({task})
 }
 
 const getTask = (req,res) => {
--- 03-task-manager/starter/models/tasks.js
+++ 03-task-manager/starter/models/tasks.js
@@ -1,3 +1,7 @@
-const mongoose - require('mongoose')
+const mongoose = require('mongoose')
 
-const taskSchema = new mongoose.Schema({})
+const TaskSchema = new mongoose.Schema({
+    name:String,completed:Boolean
+})
+
+module.exports = mongoose.model('Task', TaskSchema)
