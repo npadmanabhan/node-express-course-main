ca0e0392cc3e7bb0036401bc42b26b517c5e328a
Nandu Padmanabhan <nandu77@gmail.com>
2024-03-22 17:52:09 -0500

    Changes to app.js

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -2,7 +2,9 @@ const express = require('express')
 const app = express()
 
 const people = require('./final/13-router-people')
+const products = require('./final/16-router-products')
 const auth = require('./final/14-router-auth')
+
 
 // static assets
 app.use(express.static('./methods-public'))
@@ -11,7 +13,9 @@ app.use(express.urlencoded({ extended: false }))
 // parse json
 app.use(express.json())
 
-app.use('/api/people', people)
+app.use('/api/v1/people', people)
+app.use('/api/v1/products', products)
+
 app.use('/login', auth)
 
 app.listen(5000, () => {
